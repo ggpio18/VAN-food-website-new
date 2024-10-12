59fb710afe5c7aee8722a3bda8e34fd072d4bcae
ggpio18 <58065866+ggpio18@users.noreply.github.com>
2024-10-13 03:02:55 +0800

    counter updt

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ toggleBtn.addEventListener("click", () => {
   headerNav.classList.toggle("open");
   toggleBtn.classList.toggle("open");
 });
+
+//counter
+const counts = document.querySelectorAll('.count')
+const speed = 97
+
+counts.forEach((counter) => {
+    function upDate(){
+        const target = Number(counter.getAttribute('data-target'))
+        const count = Number(counter.innerText)
+        const inc = target / speed        
+        if(count < target){
+            counter.innerText = Math.floor(inc + count) 
+            setTimeout(upDate, 15)
+        }else{
+            counter.innerText = target
+        }
+    }
+    upDate()
+})
